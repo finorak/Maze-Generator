65aa7eb4cb08ba6038fa8bdabced8b1a271c80e9
finorak <finoana265@gmail.com>
2026-03-18 12:25:32 +0300

    nothing done

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -2,6 +2,8 @@
 The main program for our maze generator
 """
 
+from re import M
+from typing import Any
 from src.setting import WIDTH, HEIGHT, TITLE
 from mlx import Mlx
 
@@ -10,16 +12,15 @@ class Maze:
     """
     The maze class
     """
+    ROWS = 20
+    COLS = 15
+
     def __init__(self) -> None:
         """
         Initialisation of our our maze
         """
         # we use this to see if the logo can be displayed
         self.show_logo = True
-        # initializing mlx
-        self._init()
-        self.event_handler(self.mlx_window)
-        self.mlx.mlx_loop(self.mlx_ptr)
 
     def _init(self):
         self.mlx = Mlx()
@@ -28,7 +29,15 @@ class Maze:
                 self.mlx_ptr, WIDTH, HEIGHT, TITLE
                 )
 
-    def close(self, keycode: int = 33):
+    def run(self):
+        # initializing mlx
+        self._init()
+        # handling event
+        self.event_handler(self.mlx_window)
+        # looping over
+        self.mlx.mlx_loop(self.mlx_ptr)
+
+    def close_via_quit(self, keycode: int = 33):
         """
         Closing the window
         :param keycode: the keycode we recieve from mlx_hook
@@ -37,6 +46,11 @@ class Maze:
         self.mlx.mlx_destroy_window(self.mlx_ptr, self.mlx_window)
         # exiting the mlx_loop
         self.mlx.mlx_loop_exit(self.mlx_ptr)
+
+    def draw_line(self, window: Any) -> None:
+        for i in range(self.ROWS):
+            for j in range(self.COLS):
+                pass
 
     def draw(self):
         """
@@ -50,9 +64,20 @@ class Maze:
         """
         pass
 
-    def event_handler(self, window):
-        self.mlx.mlx_hook(window, 33, 0, self.close, None)
+    def handle_key(self, keycode: int) -> None:
+        print(keycode)
+        if keycode == 65307:
+            self.close_via_quit(keycode)
+
+    def event_handler(self, window: Any):
+        """
+        This function will contain all the event handler
+        """
+        self.mlx.mlx_hook(window, 3, 0, self.handle_key, None)
+        self.mlx.mlx_hook(window, 33, 0,
+                          self.close_via_quit, None)
 
 
 if __name__ == "__main__":
     maze = Maze()
+    maze.run()

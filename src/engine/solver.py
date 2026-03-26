3df7a1ce83d149ae5e0a7a59c4a8edaa18fc35f4
finorak <finoana265@gmail.com>
2026-03-26 15:39:11 +0300

    dfs algo

--- src/engine/solver.py
+++ src/engine/solver.py
@@ -1,33 +1,26 @@
-from random import shuffle
-from typing import Any
-
-from src.setting import CELL_COLOR, VISITED_COLOR
 from ..cell import Cell
 
-
 class Solver:
-    def __init__(self, data: list[list[Cell]]) -> None:
+    def __init__(
+        self, data: list[list[Cell]], 
+        ENTRY: tuple[int, int], EXIT: tuple[int, int]
+    ) -> None:
         self.data = data
-        self.result = {}
+        self.entry = ENTRY
+        self.exit = EXIT
+        self.path: list[tuple[int, int]] = []
 
-    def dfs_solver(self, curr_pos: tuple[int, int],
-                   end_pos: tuple[int, int],
-                   draw_func: Any) -> bool:
-        if curr_pos == end_pos:
+    def dfs_solver(self, curr_pos: tuple[int, int]) -> bool:
+        if curr_pos == self.exit:
+            self.path.append(curr_pos)
             return True
         curr_x, curr_y = curr_pos
-        cell = self.data[curr_x][curr_y]
-        cell.is_visited = True
-        cell.color = VISITED_COLOR
-        draw_func()
-        negithboors = cell.get_neightboors(self.data) or []
-        if negithboors:
-            shuffle(negithboors)
-        for negithboor in negithboors:
-            if negithboor.is_visited:
-                continue
-            x, y = negithboor.row, negithboor.col
-            if self.dfs_solver((x, y), end_pos, draw_func):
+        curr_cell = self.data[curr_x][curr_y]
+        curr_cell.is_visited = True
+        negightboors = self.get_neightboor(curr_cell)
+        for negightboor in negightboors:
+            x, y = negightboor.row, negightboor.col
+            if self.dfs_solver((x, y)):
+                self.path.append((x, y))
                 return True
-            self.data[x][y].color = CELL_COLOR
         return False
